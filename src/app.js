37ed80e3443a040b1d9d6633845e81fc85f9d79c
ybZhang <472099249@qq.com>
2020-09-20 23:30:25 +0800

    完成layout组件的基本功能

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Col from './col'
 import Row from './row'
+import Header from './header'
+import Footer from './footer'
+import Content from './content'
+import Layout from './layout'
+import Sider from './sider'
 
 Vue.component('z-button', Button)
 Vue.component('z-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('z-button-group', ButtonGroup)
 Vue.component('z-input', Input)
 Vue.component('z-row', Row)
 Vue.component('z-col', Col)
+Vue.component('z-header', Header)
+Vue.component('z-footer', Footer)
+Vue.component('z-content', Content)
+Vue.component('z-layout', Layout)
+Vue.component('z-sider', Sider)
 
 new Vue({
     el: '#app',
