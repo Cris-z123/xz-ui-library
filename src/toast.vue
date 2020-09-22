5edf2114b801fd334c70c2de0e1d98b27f1fc267
ybZhang <472099249@qq.com>
2020-09-22 22:15:36 +0800

    完成toast的三种动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="toast" :class="toastClasses" ref="wrapper">
-        <slot></slot>
-        <div class="line" ref="line"></div>
-        <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+    <div class="wrapper" :class="toastClasses">
+        <div class="toast" ref="toast">
+            <slot></slot>
+            <div class="line" ref="line"></div>
+            <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+        </div>
     </div>
 </template>
 
@@ -61,7 +63,7 @@
                 }, this.autoCloseDelay * 1000)
             }
             this.$nextTick(() => {
-                this.$refs.line.style.height = `${this.$refs.wrapper.getBoundingClientRect().height}px`
+                this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
             })
         }
     }
@@ -71,9 +73,56 @@
 $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, .75);
-.toast {    
+@keyframes slide-up {
+    0% {
+        opacity: 0;
+        transform: translateY(100%);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    } 
+}
+@keyframes fade-in {
+    0% { opacity: 0; }
+    100% { opacity: 1; } 
+}
+@keyframes slide-down {
+    0% {
+        opacity: 0;
+        transform: translateY(-100%);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    } 
+}
+.wrapper {
     position: fixed;
     left: 50%;
+    transform: translateX(-50%);
+    $animation-duration: 1s;
+    &.position-top {
+        top: 10px;
+        .toast {
+            animation: slide-down $animation-duration;
+        }
+    }
+    &.position-mid {
+        top: 50%;
+        transform: translate(-50%, -50%);
+        .toast {
+            animation: fade-in $animation-duration;
+        }
+    }
+    &.position-bottom {
+        bottom: 10px;
+        .toast {
+            animation: slide-up $animation-duration;
+        }
+    }
+}
+.toast {    
     display: flex;
     align-items: center;
     font-size: $font-size;
@@ -84,26 +133,14 @@ $toast-bg: rgba(0, 0, 0, .75);
     border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, .5);
     padding: 0 16px;
-    &.position-top {
-        top: 10px;
-        transform: translateX(-50%);
-    }
-    &.position-mid {
-        top: 50%;
-        transform: translate(-50%, -50%);
-    }
-    &.position-bottom {
-        bottom: 10px;
-        transform: translateX(-50%);
-    }
-}
-.close {
+    .close {
     padding-left: 16px;
     flex-shrink: 0;
-}
-.line {
+    }
+    .line {
     height: 100%;
     border-left: 1px solid #666;
     margin-left: 16px;
+    }
 }
 </style>
